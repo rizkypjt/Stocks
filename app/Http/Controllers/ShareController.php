ae14a1b88d1c9c5cce8ecb37ff2da0417ae3205f
rizkypjt <rizkyhidayatpanjaitan97@gmail.com>
2019-01-29 15:25:16 +0700

    function crud

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -18,30 +18,46 @@ class ShareController extends Controller
     }
 
     public function store(Request $request){
-        $request->validate([
-            'share_name'=>'required',
-            'share_price'=>'required|integer',
-            'share_qty' => 'required|integer'
-        ]);
-
-        $share = new Share([
-            'share_name'=>$request->get('share_name'),
-            'share_price'=>$request->get('share_price'),
-            'share_qty'=>$request->get('share_qty')
-        ]);
-        $share->save();
-        return redirect('/shares')->with('success', 'stocks has been added');
+      $request->validate([
+        'share_name'=>'required',
+        'share_price'=> 'required|integer',
+        'share_qty' => 'required|integer'
+      ]);
+      $share = new Share([
+        'share_name' => $request->get('share_name'),
+        'share_price'=> $request->get('share_price'),
+        'share_qty'=> $request->get('share_qty')
+      ]);
+      $share->save();
+      return redirect('/shares')->with('success', 'Stock has been added');
     }
 
     public function edit($id){
-        //
+        $share = Share::find($id);
+        return view('shares.edit', compact('share'));
     }
 
+    
     public function update(Request $request, $id){
-        //
+        $request->validate([
+            'share_name'=>'required',
+            'share_price'=> 'required|integer',
+            'share_qty' => 'required|integer'
+          ]);
+    
+          $share = Share::find($id);
+          $share->share_name = $request->get('share_name');
+          $share->share_price = $request->get('share_price');
+          $share->share_qty = $request->get('share_qty');
+          $share->save();
+    
+          return redirect('/shares')->with('success', 'Stock has been updated');
     }
 
     public function destroy($id){
-        //
+        $share = Share::find($id);
+        $share->delete();
+
+        return redirect('/shares')->with('success', 'Stock has been delete Successfully');
     }
 }
